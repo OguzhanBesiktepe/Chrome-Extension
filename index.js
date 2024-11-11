726a55ffa5b5ee280202db63f7ba65a2622b4d49
Oguzhan <oguzhanbesiktepe@gmail.com>
2024-11-11 17:01:00 -0500

    added chrome.tabs API allowing to grab the current tab and save it in our extension.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = []
 let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 
@@ -11,6 +12,14 @@ if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
     let listItems = ""
